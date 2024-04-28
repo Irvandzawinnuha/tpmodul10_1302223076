47b0022e34b8ff864cd17fed746070101a782e43
Irvandzawinnuha <72366333+Irvandzawinnuha@users.noreply.github.com>
2024-04-28 23:07:57 +0700

    Tambahkan implementasi Mahasiswa

---
+++ tpmodul10_1302223076/Class.cs
@@ -0,0 +1,13 @@
+﻿namespace tpmodul10_1302223076
+{
+    public class Mahasiswa
+    {
+        public String Nama { get; set; }
+        public String Nim { get; set; }
+        public Mahasiswa(String nama, String nim)
+        {
+            Nama = nama;
+            Nim = nim;
+        }
+    }
+}
---
+++ tpmodul10_1302223076/Controllers/ValuesController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Contracts;
+
+namespace tpmodul10_1302223076.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MahasiswaController : ControllerBase
+    {
+        List<Mahasiswa> LMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa("Irvan Dzawin Nuha", "1302223076"),
+            new Mahasiswa("Joshua Damiel Simanjuntak", "130222072"),
+            new Mahasiswa("Rakha Galih Nugraha Sukma", "1302223118"),
+            new Mahasiswa("Mohammed Yousef Gumilar", "1302220085"),
+            new Mahasiswa("Abdillah Aufa Tawiyya", "1302220103")
+        };
+        
+        [HttpGet]
+        public IEnumerable<Mahasiswa> Get() { return LMahasiswa; }
+
+        [HttpGet("{id}")]
+        public Mahasiswa Get(int id)
+        {
+            return LMahasiswa[id];
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Mahasiswa value)
+        {
+            LMahasiswa.Add(value);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            LMahasiswa.RemoveAt(id);
+        }
+
+    }
+}
